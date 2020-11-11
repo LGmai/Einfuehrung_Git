ac152279cad86a604d80d2b903c0043ed1319c6a
oliviad5 <odetzlho@htl-steyr.ac.at>
2020-11-11 09:25:19 +0100

    1. Änderung

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Olivia");
         System.out.println("Testversuch");
+        System.out.println("Hallo Lisa");
     }
 }
